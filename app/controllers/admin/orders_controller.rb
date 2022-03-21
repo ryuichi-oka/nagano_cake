c41a97242148fa91752e786817f6e2e5178691d4
EC2 Default User <ec2-user@ip-172-31-42-83.ec2.internal>
2022-03-21 10:08:04 +0000

    [finish]レイアウト調整

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,20 +1,23 @@
 class Admin::OrdersController < ApplicationController
 
-　def show
-　  @order = Order.find(params[:id])
-    @order_detail = @order.order_details
-　end
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.shipping_cost = "800"
+    @total_price = @order_details.sum{|order_detail|order_detail.subtotal}
+  end
 
-　def update
-　  @order = Order.find(params[:id])
-　  @order_details = @order.order_details
-　  @order.update(order_params)
-　  redirect_to admin_order_path(@order.id)
-　end
+  def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.update(order_params)
+    redirect_to admin_order_path(@order.id)
+  end
 
   private
 
   def order_params
-    params.require(:order).permit(:postal_code, :address, :name, :total_payment, :payment_method, :status, :custmoer_id)
+    params.require(:order).permit(:postal_code, :address, :name, :total_payment, :payment_method, :status)
   end
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,11 +2,11 @@ class OrderDetail < ApplicationRecord
 
   belongs_to :order
   belongs_to :item
-  
+
   def subtotal
     price * amount
   end
 
-  enum maiking_status: {cannot: 0, waiting: 1, product: 2, complete: 3}
+  enum making_status: {cannot: 0, waiting: 1, product: 2, complete: 3}
 
 end
